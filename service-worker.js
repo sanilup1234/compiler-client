105c511f3412248b8138e6ba9b21f1c85c9bd310
sanilup1234 <sanil.upadhyay12@gmail.com>
2020-05-27 01:04:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compiler-client/precache-manifest.8b9995865b856d07799ba31b007aed70.js"
+  "/compiler-client/precache-manifest.6ae1bf3729dad1266002b061f5209475.js"
 );
 
 self.addEventListener('message', (event) => {
